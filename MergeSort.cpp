dbd83763db191b18544e03184bfee0ef3fb40fc0
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-17 15:58:49 +0700

    Pembuatan Prosedur Input

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,3 +5,23 @@ int arr[20], B[20];
 
 int n;
 
+void input(){
+    while (true)
+    {
+        cout << "Masukkan Panjang element arrray: ";
+        cin >> n;
+
+        if (n<= 20)
+        {
+            break;
+        }
+        else{
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+        
+    }
+
+    cout << "\n============================";
+    cout << "\nInputkan Isi Element Array";
+    cout << "\n============================" << endl;
+}
