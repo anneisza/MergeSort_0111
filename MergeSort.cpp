01cd5832c3a15adfab04a07a3448cf8af05814d7
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-17 16:20:05 +0700

    Melengkapi step 4-5

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -68,4 +68,25 @@ void mergeSort(int low, int high){
         }
         k++; //step 4.d.ii
     }
+
+    //step 4.e
+    while (j <= high)
+    {
+        B[k] = arr[j];
+        j++;
+        k++; 
+    }
+    //step 4.f
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    //step 5
+    for (int y = low; y <= high; y++)
+    {
+        arr[y] = B[y];
+    }  
 }
